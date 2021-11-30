4ee8c22933b4bc05d2de7630b222c82b93da61c0
Ngân Nguyễn <nkn01698981443@gmail.com>
2021-11-30 09:27:56 +0700

    Update ObjectDetection

--- FaceCompare/src/DTO/Response.java
+++ FaceCompare/src/DTO/Response.java
@@ -5,6 +5,7 @@
 package DTO;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,15 +16,28 @@ public class Response implements Serializable {
     Person person;
     Photo photo;
     String message;
+    ArrayList<String> obj;
 
     public Response(Person person, Photo photo, String message) {
         this.person = person;
         this.photo = photo;
         this.message = message;
     }
-
+    
     public Response(String message) {
         this.message = message;
+    }
+    
+    public Response(ArrayList<String> obj) {
+        this.obj = obj;
+    }
+
+    public void setObj(ArrayList<String> obj) {
+        this.obj = obj;
+    }
+
+    public ArrayList<String> getObj() {
+        return obj;
     }
 
     public Person getPerson() {
